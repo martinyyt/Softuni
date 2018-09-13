128c3d3c00a15d0419b77fe0bfdc752bb8c824fd
martinyyt <martinyyt@users.noreply.github.com>
2018-09-13 16:39:36 +0300

    13.09.2018

---
+++ PrinciplesOOP/PrinciplesOOP/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Test
+{
+    class Test
+    {
+         static public int a;
+         static void Main(string[] args)
+        {
+            int a=5;
+            Console.WriteLine(Test.a);
+            // Console.WriteLine(this.a); // Must be non static
+            Console.WriteLine(a);
+
+            int[] arr = new int[5];
+            arr = new int[6];
+        }
+    }
+}
