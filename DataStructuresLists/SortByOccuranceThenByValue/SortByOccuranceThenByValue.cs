a13d5312cc67f7d095df46efad2254cbc8295c56
martinyyt <martinyyt@users.noreply.github.com>
2018-10-10 17:14:37 +0300

    some sorting

---
+++ DataStructuresLists/SortByOccuranceThenByValue/SortByOccuranceThenByValue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortByOccuranceThenByValue
+{
+    class SortByOccuranceThenByValue
+    {
+        static SortedDictionary<int, int> CustomSort(List<int> numbers)
+        {
+            SortedDictionary<int, int> numberOccurances = new SortedDictionary<int, int>();
+            foreach (var number in numbers)
+            {
+                if (numberOccurances.TryGetValue(number, out int value))
+                {
+                    numberOccurances[number] = value + 1;
+                }
+                else
+                {
+                    numberOccurances.Add(number, 1);
+                }
+
+            }
+            return numberOccurances;
+        }
+        static List<int> FinalSort(SortedDictionary<int, int> numbersByOccurance)
+        {
+            var sortedNumbersDict = numbersByOccurance.OrderBy(x => x.Value);
+            var sortedNumbers = new List<int>();
+            foreach (var pair in sortedNumbersDict)
+            {
+                for (int i = 0; i < pair.Value; i++)
+                {
+                    sortedNumbers.Add(pair.Key);
+                }
+            }
+            return sortedNumbers;
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 2, 5, 6, 8, 5, 8, 6, 4, 8, 5, 6, 8, 5, 2, 4, 5, 2, 24, 86, 7 };
+            //var sl = numbers.OrderBy(x => numbers.Count(p => p == x)).ThenBy(c => c);
+            //foreach (var item in sl)
+            //{
+            //    Console.WriteLine(item);
+            //} // short solution here!!! complex lambda!
+            SortedDictionary<int, int> numbersByOccurance = CustomSort(numbers);
+            var sortedNumbers = FinalSort(numbersByOccurance);
+            
+            
+
+            foreach (var item in numbersByOccurance)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            
+
+            foreach (var item in sortedNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ DataStructuresLists/SortLinkedList/SelectionSortLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortLinkedList
+{
+    class SelectionSortLinkedList
+
+    {
+        static void SelectionSort(LinkedList<int> numbers)
+        {
+
+            int temp = 0;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers.ElementAt(i) > numbers.ElementAt(j))
+                    {
+                        LinkedListNode<int> currenti = numbers.Find(numbers.ElementAt(i));
+                        LinkedListNode<int> currentj = numbers.FindLast(numbers.ElementAt(j)); //important to find last element!!!
+                        temp = currenti.Value;
+                        currenti.Value = currentj.Value;
+                        currentj.Value = temp;
+
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            List<int> numberslist = new List<int>() { 5, 8, 6, 4, 2, 65, 5, 8, 6, 3, 2, 15, 8, 5, 4, 2, 0, 69, 4, 52, 48, 145, 2, 54, 86, 5, 5 };
+            LinkedList<int> numbers = new LinkedList<int>(numberslist);
+
+
+            SelectionSort(numbers);
+
+            foreach (var numb in numbers)
+            {
+                Console.WriteLine(numb);
+            }
+
+        }
+    }
+}
